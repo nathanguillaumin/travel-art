8ebdf6ab13a622ef76fa1e8df6176d23e4b4400c
Sora952 <sora952c16b10@gmail.com>
2020-04-29 16:23:42 +0200

    adding border radius img

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import './App.css';
-import PaintingsCards from './components/PaintingsCards';
 
 function App () {
   return (
     <div className='App'>
-      <PaintingsCards />
-      <PaintingsCards />
-      <PaintingsCards />
-      <PaintingsCards />
-      <PaintingsCards />
-      <PaintingsCards />
     </div>
   );
 }
