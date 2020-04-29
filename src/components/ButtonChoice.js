c2eaad08f5e0ce4d4099557bbc066144f180ded1
Adama SONKO <thioff1@gmail.com>
2020-04-29 18:53:58 +0200

    integration home

--- src/components/ButtonChoice.js
+++ src/components/ButtonChoice.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import ButtonStyle from './ButtonStyle.css';
 
 class ButtonChoice extends Component {
   render () {
     return (
       <div>
-        <button value='American Decorative Arts' />
+        <button className="Button1">American Decorative Art</button>
+        <button className="Button2">Arts of Africa, Oceania, and the Americas</button>
+        <button className="Button3">Asian Art</button>
+        <button className="Button4">Egyptian Art</button>
+        <button className="Button5">European Painting</button>
+        <button className="Button6">European Sculpture and Decorative Arts</button>
+        <button className="Button7">Greek and Roman Art</button>
       </div>
     );
   }
 }
 
-export default Button - choice;
+export default ButtonChoice;
